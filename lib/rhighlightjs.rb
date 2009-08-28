833c4a100ec75ab92e0d42a9486f50c1d69ca087
romanvbabenko <romanvbabenko@gmail.com>
2009-08-28 10:44:27 +0300

    [^] Code cleanup

--- lib/rhighlightjs.rb
+++ lib/rhighlightjs.rb
@@ -2,7 +2,7 @@ module Rubyhammer
   module RHighlightJSHelper
     def highlighter(style='default', options={:languages=>""})
       html =  stylesheet_link_tag(style) 
-      html += "<script src=\"/javascripts/highlight.js\"></script>"
+      html += "<script script type=\"text/javascript\" src=\"/javascripts/highlight.js\"></script>"
       html += "<script>initHighlightingOnLoad(#{options[:languages]});</script>"
     end
   end
